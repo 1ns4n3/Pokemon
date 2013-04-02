767fd260d43f54fd0d8cb576709426e75626f9c4
giveli <elise.givelet@gmail.com>
2013-04-02 10:58:26 +0200

    F15 avec BD

--- src/main/java/fr/univaix/iut/pokebattle/smartcell/CaractCell.java
+++ src/main/java/fr/univaix/iut/pokebattle/smartcell/CaractCell.java
@@ -1,11 +1,25 @@
 package fr.univaix.iut.pokebattle.smartcell;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import fr.univaix.iut.pokebattle.twitter.Tweet;
+import fr.univaix.iut.progbd.DAOPokemonJPA;
+import fr.univaix.iut.progbd.Pokemon;
 
 public class CaractCell implements SmartCell {
+	
+	private EntityManagerFactory emf; 
+	private EntityManager em;
+	
+	public CaractCell() {
+    	emf = Persistence.createEntityManagerFactory("pokebattlePU");
+        em = emf.createEntityManager();
+	}
 
 	@Override
-	public String ask(Tweet question) {
+	public String ask(Tweet tweet) {
 		
 		// 	ex :
 		// 	pcreux: "@bulbizare1 #stat #level ?"
@@ -18,18 +32,25 @@ public class CaractCell implements SmartCell {
 		// bulbizare1: "@pcreux #PV=10/100"
 
 		
-		if(question.getText().contains("#stat"))
+		if(tweet.getText().contains("#stat"))
 		{	
-			if (question.getText().contains("#level")){
-				String answer = "@" + question.getScreenName() + "#level=1"/* + getLevel()*/;
+			DAOPokemonJPA dao = new DAOPokemonJPA(em);
+			
+        	String[] alias = tweet.getText().split(" ");
+         	String pokemon = alias[0].toUpperCase();
+         	pokemon = pokemon.substring(1, pokemon.length());
+			Pokemon poke = dao.getById(pokemon);
+			
+			if (tweet.getText().contains("#level")){
+				String answer = "@" + tweet.getScreenName() + " #level="+ poke.getLevel();
 				return answer;
 			}
-			if (question.getText().contains("#XP")){
-				String answer = "@" + question.getScreenName() + "#XP=0"/* + getXp()*/;
+			if (tweet.getText().contains("#XP")){
+				String answer = "@" + tweet.getScreenName() + " #XP=" + poke.getXp();
 				return answer;
 			}
-			if (question.getText().contains("#PV")){
-				String answer = "@" + question.getScreenName() + "#PV=10/100"/* + getPv()*/;
+			if (tweet.getText().contains("#PV")){
+				String answer = "@" + tweet.getScreenName() + " #PV=" + poke.getPv() + "/" + poke.getBaseHP();
 				return answer;
 			}
 		}
