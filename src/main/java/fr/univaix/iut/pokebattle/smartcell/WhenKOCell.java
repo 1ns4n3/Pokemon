1cd3fdd1f03780b6a68288069864ab84251f249a
g1103283 <g1103283@squeeze.iut.univ-aix.fr>
2013-04-03 10:41:08 +0200

    F15 - OK avec BD et test

--- src/main/java/fr/univaix/iut/pokebattle/smartcell/WhenKOCell.java
+++ src/main/java/fr/univaix/iut/pokebattle/smartcell/WhenKOCell.java
@@ -1,9 +1,23 @@
 package fr.univaix.iut.pokebattle.smartcell;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import fr.univaix.iut.pokebattle.twitter.Tweet;
+import fr.univaix.iut.progbd.DAOPokemonJPA;
+import fr.univaix.iut.progbd.Pokemon;
 
 public class WhenKOCell implements SmartCell {
 
+	private EntityManagerFactory emf; 
+	private EntityManager em;
+	
+	public WhenKOCell() {
+    	emf = Persistence.createEntityManagerFactory("pokebattlePU");
+        em = emf.createEntityManager();
+	}
+	
 	@Override
 	public String ask(Tweet tweet) {
 		
@@ -22,15 +36,30 @@ public class WhenKOCell implements SmartCell {
 		//String[] tabMots = texte.split(" ");
 		
 		//if(tabMots[0] est un poké et que ses PV <= 0)
-		{	
+		/*{	
 			//String pokeKO = tabMots[0];
 			
-			String answer = "#KO /cc " /* + @JUGE + */+ " "
-					/* + "@DRESSEURVAINQUEUR" + "@PRORIOPOKEKO"*/ ;
+			String answer = "#KO /cc " /* + @JUGE + + " "
+					/* + "@DRESSEURVAINQUEUR" + "@PRORIOPOKEKO" ;
+			return answer;
+		}*/
+		
+		DAOPokemonJPA dao = new DAOPokemonJPA(em);
+			
+    	String[] alias = tweet.getText().split(" ");
+     	String pokemon = alias[0].toUpperCase();
+     	pokemon = pokemon.substring(1, pokemon.length());
+		Pokemon poke = dao.getById(pokemon);
+		
+		if (poke.getPv() == 0) 
+		{
+			// Juge
+			String answer = "#KO /cc @viviane @" + poke.getOwner_poke() + " @"; //indiquer nom du dernier éleveur qui a attaqué poke
 			return answer;
 		}
-		
-		//return null;
+		else 
+			return null;
+
 	}
 
 }
